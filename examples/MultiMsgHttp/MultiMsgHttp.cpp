8d7a732f9cbe58336daed83eb3cf6cb69ad61c84
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-05-09 16:42:40 -0300

    feat: added example to use of httpServer com module

---
+++ examples/MultiMsgHttp/MultiMsgHttp.cpp
@@ -0,0 +1,84 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "Arduino.h"
+#include "ArduinoJson.h"
+#include "Communication.h"
+#include "DigitalControlData.hpp"
+#include "PwmControlData.hpp"
+#include "ESP8266WiFi.h"
+
+enum MsgId
+{
+  None,
+  Digital,
+  Pwm
+};
+
+DigitalControlData digitalControl;
+PwmControlData pwmControl;
+JsonMsg control;
+CommunicationHttpServer comm;
+
+AsyncWebServer server(80);
+
+const char ssid[] = "cabo canaveral 2g";
+const char pass[] = "16192020";
+
+void connect(){
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+}
+
+void setup() {
+  Serial.begin(74400);
+  WiFi.begin(ssid, pass);
+
+  connect();
+  Serial.println("connected!");
+
+  comm.init(&server);
+  Serial.print(WiFi.localIP());
+  Serial.println(comm.getUri());
+}
+
+void loop() {
+  comm.task();
+
+  if (comm.dataAvailable())
+  {
+    String data = comm.getString();
+    control.fromJson(data);
+    Serial.println(data);
+
+    switch (control.msgId)
+    {
+    case MsgId::None:
+      
+      break;
+    case MsgId::Digital:
+      digitalControl.fromJson(data);
+      pinMode(digitalControl.pin, OUTPUT);
+      digitalWrite(digitalControl.pin, digitalControl.status);
+      break;
+    case MsgId::Pwm:
+      pwmControl.fromJson(data);
+      pinMode(pwmControl.pin, OUTPUT);
+      analogWrite(pwmControl.pin, pwmControl.value);
+      break;
+    default:
+      break;
+    }
+  }
+}
